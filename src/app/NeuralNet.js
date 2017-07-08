09a770ac4147eb1c8e3e88bc73b310aec984cc59
Kyle Miller <kylemiller457@gmail.com>
2017-07-07 19:16:59 -0500

    A lot I will write everything out in the next commit when i merge to develop.

--- src/app/NeuralNet.js
+++ src/app/NeuralNet.js
@@ -2,8 +2,6 @@
  * Created by Kyle on 6/23/2017.
  */
  export default class NeuralNet {
-
-
     inputLayer;
     hiddenLayer;
     outputLayer;
@@ -42,18 +40,18 @@
 
     feedForward(){
         //this is the process of feeding the data forward from input to the output of the graph.
-        this.hiddenLayer = dotProduct(this.inputLayer, this.syn0);
+        this.hiddenLayer = this.dotProduct(this.inputLayer, this.syn0);
         for (let i = 0; i < this.hiddenLayer.length; i++){
             for (let j = 0; j < this.hiddenLayer[i].length; j ++){
-                this.hiddenLayer[i][j] = sigmoid(this.hiddenLayer[i][j]);
+                this.hiddenLayer[i][j] = this.sigmoid(this.hiddenLayer[i][j]);
             }
         }
 
         // feed into the output layer
-        this.outputLayer = dotProduct(this.hiddenLayer, this.syn1);
+        this.outputLayer = this.dotProduct(this.hiddenLayer, this.syn1);
         for (let i = 0; i < this.outputLayer.length; i++){
             for (let j = 0; j < this.outputLayer[i].length; j ++){
-                this.outputLayer[i][j] = sigmoid(this.outputLayer[i][j]);
+                this.outputLayer[i][j] = this.sigmoid(this.outputLayer[i][j]);
             }
         }
     }
@@ -70,17 +68,17 @@
         this.l2_delta = [[],[],[],[]];
         for(let i = 0; i < this.outputLayer.length; i++){
             for(let j = 0; j < this.outputLayer[i].length; j++){
-                this.outputDeriv[i][j] = sigmoid(this.outputLayer[i][j], true);
+                this.outputDeriv[i][j] = this.sigmoid(this.outputLayer[i][j], true);
                 this.l2_delta[i][j] = this.l2_error[i][j] * this.outputDeriv[i][j];
             }
         }
 
-        this.l1_error = dotProduct(this.l2_delta, transposeMat(this.syn1));
+        this.l1_error = this.dotProduct(this.l2_delta, this.transposeMat(this.syn1));
         this.hiddenDeriv = [[],[],[],[]];
         this.l1_delta = [[],[],[],[]];
         for(let i = 0; i < this.hiddenLayer.length; i++){
             for(let j = 0; j < this.hiddenLayer[i].length; j++){
-                this.hiddenDeriv[i][j] = sigmoid(this.hiddenLayer[i][j], true);
+                this.hiddenDeriv[i][j] = this.sigmoid(this.hiddenLayer[i][j], true);
                 this.l1_delta[i][j] = this.l1_error[i][j] * this.hiddenDeriv[i][j];
             }
         }
@@ -89,10 +87,10 @@
     updateWeights(){
 
 
-        this.deltaSyn1 = dotProduct(transposeMat(this.l2_delta), this.hiddenLayer);
-        this.deltaSyn0 = dotProduct(transposeMat(this.l1_delta), this.inputLayer);
-        this.deltaSyn1 = transposeMat(this.deltaSyn1);
-        this.deltaSyn0 = transposeMat(this.deltaSyn0);
+        this.deltaSyn1 = this.dotProduct(this.transposeMat(this.l2_delta), this.hiddenLayer);
+        this.deltaSyn0 = this.dotProduct(this.transposeMat(this.l1_delta), this.inputLayer);
+        this.deltaSyn1 = this.transposeMat(this.deltaSyn1);
+        this.deltaSyn0 = this.transposeMat(this.deltaSyn0);
         for( let i = 0; i < this.syn1.length; i++){
             for ( let j = 0; j < this.syn1[i].length; j++){
                 this.syn1[i][j] += this.deltaSyn1[i][j];
@@ -106,78 +104,77 @@
         }
     }
 
-    train(){
-        for(let i = 0; i < 1000; i++){
+    train(x){
+        for(let i = 0; i < x; i++){
             this.feedForward();
             this.calcError();
             this.updateWeights();
             console.log(this.outputLayer);
         }
     }
-}
 
+    changeActivation(x){
+        this.activationFucntion = x;
+    }
 
-function dotProduct(A, B){
-    if(!(A[0].length === B.length)){
-        console.log("The Dimensions of your Matrices are off. ");
-        return null;
-    }else{
+    dotProduct(A, B){
+        if(!(A[0].length === B.length)){
+            console.log("The Dimensions of your Matrices are off. ");
+            return null;
+        }else{
+            let result = [];
+            let tempRow = [];
+            let sum = 0;
+            for(let i = 0; i < A.length; i++){
+
+                for(let j = 0; j < B[0].length; j++){
+                    for(let k = 0; k < B.length; k++){
+                        sum += (A[i][k] * B[k][j]);
+                    }
+                    tempRow.push(sum);
+                    sum = 0;
+                }
+                result.push(tempRow);
+                tempRow = [];
+            }
+            return result;
+        }
+    }
+    transposeMat(A){
+        let row = A.length;
+        let col = A[0].length;
         let result = [];
         let tempRow = [];
-        let sum = 0;
-        for(let i = 0; i < A.length; i++){
-
-            for(let j = 0; j < B[0].length; j++){
-                for(let k = 0; k < B.length; k++){
-                    sum += (A[i][k] * B[k][j]);
-                }
-                tempRow.push(sum);
-                sum = 0;
+        for(let j = 0; j < col; j++){
+            for(let i = 0; i < row; i++){
+                tempRow.push(A[i][j])
             }
             result.push(tempRow);
-            tempRow = [];
+            tempRow = []
         }
         return result;
     }
-}
 
-function transposeMat(A){
-    let row = A.length;
-    let col = A[0].length;
-    let result = [];
-    let tempRow = [];
-    for(let j = 0; j < col; j++){
-        for(let i = 0; i < row; i++){
-            tempRow.push(A[i][j])
+    sigmoid(x, deriv = false) {
+        if (deriv) {
+            return x * (1 - x);
         }
-        result.push(tempRow);
-        tempRow = []
+        return 1 / (1 + Math.exp(-x));
     }
-    return result;
-}
 
-function sigmoid(x, deriv = false) {
-    if (deriv) {
-        return x * (1 - x);
+    tanh(x, deriv = false) {
+        if (deriv) {
+            return Math.acosh(1 / x) * Math.acosh(1 / x);
+        }
+        return Math.tanh(x);
     }
-    return 1 / (1 + Math.exp(-x));
-}
 
-function tanh(x, deriv = false) {
-    if (deriv) {
-        return Math.acosh(1 / x) * Math.acosh(1 / x);
+    reLU(x) {
+        if (x > 0) {
+            return x;
+        } else {
+            return x * 0.01;
+        }
     }
-    return Math.tanh(x);
-}
 
-function reLU(x) {
-    if (x > 0) {
-        return x;
-    } else {
-        return x * 0.01;
-    }
-}
-
-function changeActivationFunction(x){
-    activationFunction = x;
 }
--- src/app/index.js
+++ src/app/index.js
@@ -6,9 +6,8 @@ import NeuralNet from "./NeuralNet";
 
 import "../css/mainPage.css"
 
-
-class Graph extends React.Component {
-    constructor (props) {
+class Visualizer extends React.Component {
+    constructor(props){
         super(props);
 
         this.input = [
@@ -24,111 +23,168 @@ class Graph extends React.Component {
             [1]
         ];
         this.neuralNetwork = new NeuralNet(this.input, 0, this.output);
-    }
-    render() {
-        return (
 
-            <div className="row">
-                <button id="runButton">cool guy</button>
-                <div className="col-md-2"></div>
-                <div id="Layer" className="col-md-1">
-                    <h4>Input Layer</h4>
-                    <br />
-                    <br />
-                    <div>{this.neuralNetwork.inputLayer[0][0]}, {this.neuralNetwork.inputLayer[0][1]}, {this.neuralNetwork.inputLayer[0][2]}</div>
-                    <br />
-                    <div>{this.neuralNetwork.inputLayer[1][0]}, {this.neuralNetwork.inputLayer[1][1]}, {this.neuralNetwork.inputLayer[1][2]}</div>
-                    <br />
-                    <div>{this.neuralNetwork.inputLayer[2][0]}, {this.neuralNetwork.inputLayer[2][1]}, {this.neuralNetwork.inputLayer[2][2]}</div>
-                    <br />
-                    <div>{this.neuralNetwork.inputLayer[3][0]}, {this.neuralNetwork.inputLayer[3][1]}, {this.neuralNetwork.inputLayer[3][2]}</div>
-                </div>
-                <div id="Layer" className="col-md-2">
-                    <h4>syn0</h4>
-                    <br /><br /><br />
-                    <div>{this.neuralNetwork.syn0[0][0].toFixed(3)}, {this.neuralNetwork.syn0[0][1].toFixed(3)}, {this.neuralNetwork.syn0[0][2].toFixed(3)}, {this.neuralNetwork.syn0[0][3].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.syn0[1][0].toFixed(3)}, {this.neuralNetwork.syn0[1][1].toFixed(3)}, {this.neuralNetwork.syn0[1][2].toFixed(3)}, {this.neuralNetwork.syn0[1][3].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.syn0[2][0].toFixed(3)}, {this.neuralNetwork.syn0[2][1].toFixed(3)}, {this.neuralNetwork.syn0[2][2].toFixed(3)}, {this.neuralNetwork.syn0[2][3].toFixed(3)}</div>
-                </div>
-                <div id="Layer" className="col-md-2">
-                    <h4>Hidden Layer</h4>
-                    <br /><br />
-                    <div>{this.neuralNetwork.hiddenLayer[0][0].toFixed(3)}, {this.neuralNetwork.hiddenLayer[0][1].toFixed(3)}, {this.neuralNetwork.hiddenLayer[0][2].toFixed(3)}, {this.neuralNetwork.hiddenLayer[0][3].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.hiddenLayer[1][0].toFixed(3)}, {this.neuralNetwork.hiddenLayer[1][1].toFixed(3)}, {this.neuralNetwork.hiddenLayer[1][2].toFixed(3)}, {this.neuralNetwork.hiddenLayer[1][3].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.hiddenLayer[2][0].toFixed(3)}, {this.neuralNetwork.hiddenLayer[2][1].toFixed(3)}, {this.neuralNetwork.hiddenLayer[2][2].toFixed(3)}, {this.neuralNetwork.hiddenLayer[2][3].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.hiddenLayer[3][0].toFixed(3)}, {this.neuralNetwork.hiddenLayer[3][1].toFixed(3)}, {this.neuralNetwork.hiddenLayer[3][2].toFixed(3)}, {this.neuralNetwork.hiddenLayer[3][3].toFixed(3)}</div>
-                </div>
-                <div id="Layer" className="col-md-1">
-                    <h4>syn1</h4>
-                    <br /><br />
-                    <div>{this.neuralNetwork.syn1[0][0].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.syn1[1][0].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.syn1[2][0].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.syn1[3][0].toFixed(3)}</div>
-                </div>
-                <div id="Layer" className="col-md-1">
-                    <h4>Output Layer</h4>
-                    <br /><br />
-                    <div>{this.neuralNetwork.outputLayer[0][0].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.outputLayer[1][0].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.outputLayer[2][0].toFixed(3)}</div>
-                    <br />
-                    <div>{this.neuralNetwork.outputLayer[3][0].toFixed(3)}</div>
-                </div>
-                <div id="Layer" className="col-md-1">
-                    <h4>Expected Output</h4>
-                    <br />
-                    <div>0</div>
-                    <br />
-                    <div>0</div>
-                    <br />
-                    <div>1</div>
-                    <br />
-                    <div>1</div>
-                </div>
-                <div className="col-md-1"></div>
-            </div>
-        )
+        this.state = {
+            hiddenLayer: this.neuralNetwork.hiddenLayer,
+            syn0: this.neuralNetwork.syn0,
+            syn1: this.neuralNetwork.syn1,
+            outputLayer: this.neuralNetwork.outputLayer,
+            error: [[0],[0],[0],[0]],
+            stepInProcess: 0,
+            actImage: "Sigmoid"
+        }
     }
-}
 
-class Visualizer extends React.Component {
-    constructor(props){
-        super(props);
+    runNet(){
+        this.neuralNetwork.train(500);
+        this.setState({hiddenLayer: this.neuralNetwork.hiddenLayer});
+        this.setState({syn0: this.neuralNetwork.syn0});
+        this.setState({syn1: this.neuralNetwork.syn1});
+        this.setState({outputLayer: this.neuralNetwork.outputLayer});
+        this.setState({error: this.neuralNetwork.l2_error});
     }
+
+    handleActivationChange(x){
+        this.neuralNetwork.changeActivation(x);
+        if(x === 0){
+            this.setState({actImage: "Sigmoid"});
+        } else if (x === 1) {
+            this.setState({actImage: "tanh"});
+        } else if (x === 2) {
+            this.setState({actImage: "reLU"});
+        }
+    }
+
+    handleStepingThruNetwork(){
+        if(this.state.stepInProcess === 0){
+            this.neuralNetwork.feedForward();
+            this.setState({hiddenLayer: this.neuralNetwork.hiddenLayer});
+            this.setState({outputLayer: this.neuralNetwork.outputLayer});
+            this.setState({stepInProcess: 1});
+        } else if( this.state.stepInProcess === 1){
+            this.neuralNetwork.calcError();
+            this.setState({stepInProcess: 2});
+            this.setState({error: this.neuralNetwork.l2_error});
+        } else {
+            this.neuralNetwork.updateWeights();
+            this.setState({syn0: this.neuralNetwork.syn0});
+            this.setState({syn1: this.neuralNetwork.syn1});
+            this.setState({stepInProcess: 0});
+        }
+    }
+
+    handleReset(){
+        delete this.neuralNetwork;
+        this.neuralNetwork = new NeuralNet(this.input, 0, this.output);
+        this.setState({hiddenLayer: this.neuralNetwork.hiddenLayer});
+        this.setState({syn0: this.neuralNetwork.syn0});
+        this.setState({syn1: this.neuralNetwork.syn1});
+        this.setState({outputLayer: this.neuralNetwork.outputLayer});
+        this.setState({error: [[0],[0],[0],[0]]})
+    }
+
     render(){
         return (
             <div>
                 <h2 id="NetHeader">The Neural Network</h2>
                 <div className="row">
-                    <div id="NetHeader"  className="col-md-6">
+                    <div id="NetHeader"  className="col-md-4">
                         <h3>Controls</h3>
-                        <button id="controlButton">Step</button>
-                        <button id="controlButton">Run</button>
-                        <button id="controlButton">Stop</button>
+                        <button id="controlButton" onClick={ () => this.handleStepingThruNetwork()}>Step</button>
+                        <button id="controlButton" onClick={ () => this.runNet()}>Run</button>
+                        <button id="controlButton" onClick={ () => this.handleReset()}>Reset</button>
                     </div>
 
-                    <div id="NetHeader" className="col-md-6">
-                        <h3>Activation Functions</h3>
-                        <button id="controlButton">sigmoid</button>
-                        <button id="controlButton">tanh</button>
-                        <button id="controlButton">reLU</button>
+                    <div className="col-md-4">
+                        <h3 id="imageContent">Current Activation Function</h3>
+                        <p id="imageContent"><b>{this.state.actImage}</b></p>
+                        <img id="actImage" src={"../images/" + this.state.actImage + ".png"} />
                     </div>
-                    <Graph
-                    />
+
+                    <div id="NetHeader" className="col-md-4">
+                        <h3>Activation Functions</h3>
+                        <button id="controlButton" onClick={() => this.handleActivationChange(0)} >sigmoid</button>
+                        <button id="controlButton" onClick={() => this.handleActivationChange(1)} >tanh</button>
+                        <button id="controlButton" onClick={() => this.handleActivationChange(2)} >reLU</button>
+                    </div>
                 </div>
 
-            </div>
+                    <div className="row">
+                        <div className="col-md-2"></div>
+                        <div id="Layer" className="col-md-1">
+                            <h4>Input Layer</h4>
+                            <br />
+                            <br />
+                            <div>{this.neuralNetwork.inputLayer[0][0]}, {this.neuralNetwork.inputLayer[0][1]}, {this.neuralNetwork.inputLayer[0][2]}</div>
+                            <br />
+                            <div>{this.neuralNetwork.inputLayer[1][0]}, {this.neuralNetwork.inputLayer[1][1]}, {this.neuralNetwork.inputLayer[1][2]}</div>
+                            <br />
+                            <div>{this.neuralNetwork.inputLayer[2][0]}, {this.neuralNetwork.inputLayer[2][1]}, {this.neuralNetwork.inputLayer[2][2]}</div>
+                            <br />
+                            <div>{this.neuralNetwork.inputLayer[3][0]}, {this.neuralNetwork.inputLayer[3][1]}, {this.neuralNetwork.inputLayer[3][2]}</div>
+                        </div>
+                        <div id="Layer" className="col-md-2">
+                            <h4>syn0</h4>
+                            <br /><br /><br />
+                            <div>{this.state.syn0[0][0].toFixed(3)}, {this.state.syn0[0][1].toFixed(3)}, {this.state.syn0[0][2].toFixed(3)}, {this.state.syn0[0][3].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.syn0[1][0].toFixed(3)}, {this.state.syn0[1][1].toFixed(3)}, {this.state.syn0[1][2].toFixed(3)}, {this.state.syn0[1][3].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.syn0[2][0].toFixed(3)}, {this.state.syn0[2][1].toFixed(3)}, {this.state.syn0[2][2].toFixed(3)}, {this.state.syn0[2][3].toFixed(3)}</div>
+                        </div>
+                        <div id="Layer" className="col-md-2">
+                            <h4>Hidden Layer</h4>
+                            <br /><br />
+                            <div>{this.state.hiddenLayer[0][0].toFixed(3)}, {this.state.hiddenLayer[0][1].toFixed(3)}, {this.state.hiddenLayer[0][2].toFixed(3)}, {this.state.hiddenLayer[0][3].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.hiddenLayer[1][0].toFixed(3)}, {this.state.hiddenLayer[1][1].toFixed(3)}, {this.state.hiddenLayer[1][2].toFixed(3)}, {this.state.hiddenLayer[1][3].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.hiddenLayer[2][0].toFixed(3)}, {this.state.hiddenLayer[2][1].toFixed(3)}, {this.state.hiddenLayer[2][2].toFixed(3)}, {this.state.hiddenLayer[2][3].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.hiddenLayer[3][0].toFixed(3)}, {this.state.hiddenLayer[3][1].toFixed(3)}, {this.state.hiddenLayer[3][2].toFixed(3)}, {this.state.hiddenLayer[3][3].toFixed(3)}</div>
+                        </div>
+                        <div id="Layer" className="col-md-1">
+                            <h4>syn1</h4>
+                            <br /><br />
+                            <div>{this.state.syn1[0][0].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.syn1[1][0].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.syn1[2][0].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.syn1[3][0].toFixed(3)}</div>
+                        </div>
+                        <div id="Layer" className="col-md-1">
+                            <h4>Output Layer</h4>
+                            <br /><br />
+                            <div>{this.state.outputLayer[0][0].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.outputLayer[1][0].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.outputLayer[2][0].toFixed(3)}</div>
+                            <br />
+                            <div>{this.state.outputLayer[3][0].toFixed(3)}</div>
+                        </div>
+                        <div id="Layer" className="col-md-1">
+                            <h4>Expected Output</h4>
+                            <br />
+                            <div>0</div>
+                            <br />
+                            <div>0</div>
+                            <br />
+                            <div>1</div>
+                            <br />
+                            <div>1</div>
+                        </div>
+                        <div className="col-md-1"></div>
+                    </div>
+                    <div id="errorRow" className="row">
+                        <h4>Error</h4>
+                        <div>{(this.state.error[0][0] * 100).toFixed(3)}%, {(this.state.error[1][0] * 100).toFixed(3)}%, {(this.state.error[2][0] * 100).toFixed(3)}%, {(this.state.error[3][0] * 100).toFixed(3)}%</div>
+                    </div>
+                </div>
+
+
         );
     }
 }
